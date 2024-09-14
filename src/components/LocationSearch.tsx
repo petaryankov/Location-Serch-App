ef8a5a7ee18b6f134e14a591cf7b2868952dc345
Petar Yankov <yankov.petar4@gmail.com>
2024-09-14 19:04:58 +0300

    callback added

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,4 +1,12 @@
-import type { Place } from "../api/Place"
-export default function LocationSearch () {
+import type { Place } from "../api/Place";
+import { useState } from "react";
+
+interface LocationSearchProps {
+    onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+    const [place, setPlace] = useState<Place[]>([]);
+
     return <div>Search for a location</div>
 }
